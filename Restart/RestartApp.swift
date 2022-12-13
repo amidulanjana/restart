ebf754266c9267360087624380f4fd99a235d011
Amila Dulanjana <amiladulanjana@hotmail.com>
2022-12-13 18:58:19 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Amila Dulanjana on 2022-12-13.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
